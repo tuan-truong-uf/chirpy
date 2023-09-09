d3727ffeed64c839b9bc1e79bcf68e7f815c303b
tuan-truong-uf <git@github.com>
2023-09-09 17:36:53 -0400

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -18,6 +19,11 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>
           {!user.isSignedIn && <SignInButton />}{!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+          <div key={post.id}>{post.content}</div>
+          ))}
         </div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       </main>
