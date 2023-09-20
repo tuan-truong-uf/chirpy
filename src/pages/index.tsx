b33f4cca0b2f210448bd1604faef27e83c3605e4
tuan-truong-uf <tuantruong@ufl.edu>
2023-09-20 09:42:09 -0400

    New files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -100,9 +100,15 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex text-slate-300 gap-1">
-          <span>{`@${author.username} `}</span>
-          <span className="font-thin">{` · ${dayjs(post.createdAt).fromNow()}`}
-          </span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username} `}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{` · ${dayjs(
+              post.createdAt
+              ).fromNow()}`}
+            </span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
